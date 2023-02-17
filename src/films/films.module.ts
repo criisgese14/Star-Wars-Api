ca1d387d8600c034e9e77623aa84d02dc361c173
criisgese14 <cristiangsosa15@gmail.com>
2023-02-17 14:21:28 -0300

    creación de schema film

--- src/films/films.module.ts
+++ src/films/films.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { FilmsController } from './films.controller';
 import { FilmsService } from './films.service';
+import { Film, FilmSchema } from './schema/films.schema';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([{ name: Film.name, schema: FilmSchema}])],
   controllers: [FilmsController],
   providers: [FilmsService]
 })
---
+++ src/films/schema/films.schema.ts
@@ -0,0 +1,27 @@
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type FilmDocument = HydratedDocument<Film>;
+
+@Schema()
+export class Film {
+    @Prop()
+    title: string;
+
+    @Prop({unique: true})
+    episode_id: number;
+
+    @Prop()
+    opening_crawl: string;
+
+    @Prop()
+    director: string;
+
+    @Prop()
+    producer: string;
+
+    @Prop()
+    release_date: Date;
+}
+
+export const FilmSchema = SchemaFactory.createForClass(Film);
