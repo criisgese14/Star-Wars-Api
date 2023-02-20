1422cfb7ea38cae8c03235ab7099c71606db4f67
criisgese14 <cristiangsosa15@gmail.com>
2023-02-20 19:34:08 -0300

    tipado en las funciones añadido

--- src/planets/planets.controller.ts
+++ src/planets/planets.controller.ts
@@ -1,32 +1,36 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreatePlanetDto } from './dto/createPlanet.dto';
+import { EditPlanetDto } from './dto/editPlanet.dto';
+import { deletePlanet } from './interface/deletePlanet.interface';
 import { PlanetsService } from './planets.service';
+import { PlanetDocument } from './schema/planets.schema';
 
 @Controller('planets')
 export class PlanetsController {
     constructor(private readonly planetsService: PlanetsService) {};
 
     @Get()
-    getAllPlanets() {
+    getAllPlanets(): Promise<PlanetDocument[]> {
         return this.planetsService.getAllPlanets();
     };
 
     @Get('/:id')
-    getPlanet(@Param('id') planetId: string) {
+    getPlanet(@Param('id') planetId: string): Promise<PlanetDocument> {
         return this.planetsService.getPlanet(planetId);
     };
 
     @Post()
-    createPlanet(@Body() newPlanet) {
+    createPlanet(@Body() newPlanet: CreatePlanetDto): Promise<PlanetDocument> {
         return this.planetsService.createPlanet(newPlanet);
     };
 
     @Put('/:id')
-    updatePlanet(@Param('id') planetId: string, @Body() planet) {
+    updatePlanet(@Param('id') planetId: string, @Body() planet: EditPlanetDto): Promise<PlanetDocument> {
         return this.planetsService.updatePlanet(planetId, planet);
     };
 
     @Delete('/:id')
-    deletePlanet(@Param('id') planetId){
+    deletePlanet(@Param('id') planetId: string): Promise<deletePlanet>{
         return this.planetsService.deletePlanet(planetId);
     };
 };
